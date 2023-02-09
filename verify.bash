d365924ee87ab2e6fc40a81de1c3ec8933072b93
Rory Quinn <quinn.rory@gmail.com>
2023-02-09 08:22:03 +0000

    verify image via logs (#9)
    * add verify
    
    * passing test
    
    * verify before publish
    
    * simplify step

---
+++ verify.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker build . -t verify-emulator
+
+docker run --rm --env SPANNER_DATABASE_ID=db \
+  --env SPANNER_INSTANCE_ID=inst \
+  --env SPANNER_PROJECT_ID=proj \
+  --detach \
+  --name verify \
+  verify-emulator
+
+sleep 10
+
+docker logs verify &> verifylogs
+
+cat verifylogs
+
+docker stop verify > /dev/null
+
+echo verifying log output
+
+grep "instance created" verifylogs
+grep "Cloud Spanner emulator running." verifylogs
+grep "REST server listening at 0.0.0.0:9020" verifylogs
+grep "gRPC server listening at 0.0.0.0:9010" verifylogs
+grep "database created" verifylogs
+
+echo logs contain expected output
